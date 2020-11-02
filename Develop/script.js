364df6ddc5fd14e6ac627b3ee3ed1938afc2dba8
Star-Gibson <sgibsoncoding@gmail.com>
2020-11-02 13:06:14 -0500

    Added Sections 3-6

--- Develop/script.js
+++ Develop/script.js
@@ -15,7 +15,7 @@ function writePassword() {
     var confirmUppercase = confirm("Have Password Contain Uppercase Letters?");
     var confirmNumbers = confirm("Have Password Contain Numbers?");
     var confirmLowercase = confirm("Have Password Contain Lowercase Letters?");
-    var confirmSpecial = confirm("Have Password Contain Special Characters?");
+    var confirmSymbols = confirm("Have Password Contain Special Characters?");
 
     var NPwdLength;
 
@@ -28,7 +28,7 @@ function writePassword() {
     //User's Choices 
 
     // 1 - All options
-    if (NPwdLength >= 8 && NPwdLength <= 128 && confirmUppercase && confirmNumbers && confirmLowercase && confirmSpecial) {
+    if (NPwdLength >= 8 && NPwdLength <= 128 && confirmUppercase && confirmNumbers && confirmLowercase && confirmSymbols) {
       function generate(length = NPwdLength) {
         var all = upperCase + numbers + lowerCase + symbols;
         var password = '';
@@ -44,7 +44,7 @@ function writePassword() {
 
     // 2 - Uppercase, Lowercase, Numbers
     else if (NPwdLength >= 8 && NPwdLength <= 128 && confirmUppercase && confirmLowercase && confirmNumbers) {
-      function generate (length = NPwdLength) {
+      function generate(length = NPwdLength) {
         var all = upperCase + lowerCase + numbers;
         var password = '';
         for (var index = 0; index < length; index++) {
@@ -57,9 +57,65 @@ function writePassword() {
       generate(NPwdLength)
     }
 
+    // 3 - Uppercase, Lowercase, Symbols
+    else if (NPwdLength >= 8 && NPwdLength <= 128 && confirmUppercase && confirmLowercase && confirmSymbols) {
+      function generate(length = NPwdLength) {
+        var all = upperCase + lowerCase + symbols;
+        var password = '';
+        for (var index = 0; index < length; index++) {
+          var character = Math.floor(Math.random() * all.length);
+          password += all[(character, character + 1)];
+        }
+        document.getElementById("password").innerHTML = password;
+        return password;
+      }
+      generate(NPwdLength)
+    }
 
+    //4 - Uppercase, Numbers, Symbols
+    else if (NPwdLength >= 8 && NPwdLength <= 128 && confirmUppercase && confirmNumbers && confirmSymbols) {
+      function generate(length = NPwdLength) {
+        var all = upperCase + numbers + symbols;
+        var password = '';
+        for (var index = 0; index < length; index++) {
+          var character = Math.floor(Math.random() * all.length);
+          password += all[(character, character + 1)];
+        }
+        document.getElementById("password").innerHTML = password;
+        return password;
+      }
+      generate(NPwdLength)
+    }
 
+    //5 - Lowercase, Numbers, Symbols
+    else if (NPwdLength >= 8 && NPwdLength <= 128 && confirmLowercase && confirmNumbers && confirmSymbols) {
+      function generate (length = NPwdLength) {
+        var all = lowerCase + numbers + symbols;
+        var password = '';
+        for (var index = 0; index < length; index++) {
+          var character = Math.floor(Math.random() * all.length);
+          password += all[(character, character + 1)];
+        }
+        document.getElementById("password").innerHTML = password;
+        return password;
+      }
+      generate(NPwdLength)
+    }
 
+    //6 - Uppercase, Lowercase
+    else if (NPwdLength >= 8 && NPwdLength <= 128 && confirmUppercase && confirmLowercase) {
+      function generate (length = NPwdLength) {
+        var all = upperCase + lowerCase;
+        var password = '';
+        for (var index = 0; index < length; index++) {
+          var character = Math.floor(Math.random() * all.length);
+          password += all[(character, character + 1)];
+        }
+        document.getElementById("password").innerHTML = password;
+        return password;
+      }
+      generate(NPwdLength)
+    }
 
 
   }
